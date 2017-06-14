aba563339c0436bcdff50772eca396fbd733296f
MAJESTIC123 <karthiece996@gmail.com>
2017-06-14 09:32:41 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,29 @@
+package logics;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+public static void main(String[] args) {
+	
+	System.out.println("Enter the number");
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int temp;
+	temp=n;
+	int r;
+	int sum=0;
+	
+	while(n>0) {
+		r=n%10;
+		sum=(sum*10)+r;
+		n=n/10; 
+	}
+	if(sum==temp) {
+		System.out.println(temp+" is a palindrome ");
+	}
+	else {
+		System.out.println(temp+" is not a palindrome ");
+	}
+	
+}
+}
